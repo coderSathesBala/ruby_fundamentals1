750421a93411903c599c72cd639c6bdd0edad6bd
Sathes Balasingam <sathes352@gmail.com>
2017-01-17 14:49:54 -0500

    secondcommit

---
+++ exercise2.rb
@@ -0,0 +1,8 @@
+puts "I would put a tip of #{55 * 0.15} for a 55 dollar meal"
+
+puts 3.to_s + "poop"
+
+#45628 multiplied by 7839
+puts "the result of #{45628 * 7839} is created"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
